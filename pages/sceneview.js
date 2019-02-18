2b59cdee2a1762e023e504caeb8d6461fcc3a4ec
Youngjun Choi <yjaquinas2@naver.com>
2019-02-18 11:14:10 +0900

    scene update

--- pages/sceneview.js
+++ pages/sceneview.js
@@ -47,7 +47,7 @@ class SceneView extends React.Component {
                 <Entity primitive="a-light" type="ambient" intensity="2" color="white"/>
                 <Entity primitive="a-light" type="point" intensity="4" color="white"/>
                 <Entity primitive="a-sky" height="2048" radius="30" src="#skyTexture" theta-length="90" width="2048"/>
-                <Entity text={{value: 'Click sphere to move to the right\nClick and Drag box to move', align: 'center'}} position={{x: 0, y: 2, z: -1}}/>
+                <Entity text={{value: 'Aim and click sphere to move to the right\nClick and Drag box to move', align: 'center'}} position={{x: 0, y: 2, z: -1}}/>
                 
                 <Entity
                     geometry={{primitive: 'sphere'}}
@@ -56,11 +56,15 @@ class SceneView extends React.Component {
                     events={{click: this.moveToTheRight.bind(this)}}
                 />
                 <Entity
+                    id="tempBox"
                     click-drag
                     primitive="a-box"
                     position={{x: this.state.boxPosX, y: this.state.boxPosY, z: this.state.boxPosZ}}
                     rotation="0 45 0"
                     color="#4CC3D9"
+                    events={{dragend: ()=>{
+                        console.log(this.state.position)
+                    }}}
                 />
 
                 {/* <Entity geometry={{primitive: 'box'}} material={{color: 'red'}} position={{x: 0, y: 0, z: -5}}/> */}
